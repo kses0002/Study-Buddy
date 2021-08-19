8c56913623947d0bed5c02189511dd5056dfdbd9
Keshav Seshadri <kses0002@student.monash.edu>
2021-08-19 21:06:11 +1000

    Infinity scroll, but always takes user to bottom

--- src/ViewBuddies/Chat.js
+++ src/ViewBuddies/Chat.js
@@ -1,12 +1,12 @@
-import { API, Auth } from 'aws-amplify'
-import React, { useEffect, useState, useStateRef } from 'react';
+import { API, Auth, container } from 'aws-amplify'
+import React, { useEffect, useState, useRef } from 'react';
 import Button from '@material-ui/core/Button';
 import { graphqlOperation } from '@aws-amplify/api';
 import { messagesByChannelID } from '../graphql/queries';
 import { createMessage } from '../graphql/mutations';
 import { onCreateMessage } from '../graphql/subscriptions'
 import './Chat.css'
-import { Scrollbars } from 'react-custom-scrollbars';
+import InfiniteScroll from 'react-infinite-scroller';
 
 function Chat({ data, currentUserEmail }) {
 
@@ -15,11 +15,12 @@ function Chat({ data, currentUserEmail }) {
     const [userInfo, setUserInfo] = useState(null)
     const [recipientEmail = data, setRecipientEmail, emailRef] = useState()
 
+    const [page, setPage] = useState(1);
+    const [token, setToken] = useState("");
+ 
     useEffect(() => {
         setRecipientEmail(data.emailRef)
     }, [data])
-    console.log(recipientEmail)
-
 
 
     useEffect(() => {
@@ -33,11 +34,15 @@ function Chat({ data, currentUserEmail }) {
         API
             .graphql(graphqlOperation(messagesByChannelID, {
                 channelID: '1',
-                sortDirection: 'ASC'
+                sortDirection: 'DESC'
             }))
             .then((response) => {
+                let loadMore = response?.data?.messagesByChannelID?.nextToken
+                setToken(loadMore)
+
                 const items = response?.data?.messagesByChannelID?.items;
-                for (let i = 0; i < items.length; i++) {
+
+                for (let i = items.length - 1; i >= 0; i--) {
                     if ((items[i].author == currentUserEmail
                         && items[i].recepient == recipientEmail) || (items[i].recepient == currentUserEmail
                             && items[i].author == recipientEmail)) {
@@ -50,6 +55,7 @@ function Chat({ data, currentUserEmail }) {
     }, [data]);
 
     useEffect(() => {
+
         const subscription = API
             .graphql(graphqlOperation(onCreateMessage))
             .subscribe({
@@ -89,13 +95,149 @@ function Chat({ data, currentUserEmail }) {
     const AlwaysScrollToBottom = () => {
         const elementRef = React.createRef();
         useEffect(() => elementRef.current.scrollIntoView());
+        if(page>1){
+            window.scrollTo(0, 0)
+            return <div ref={elementRef} />;
+        }
+        else{
         return <div ref={elementRef} />;
-      };
+        }
+        return <div ref={elementRef} />;
+    };
+
+    const scrollToBottom = () => {
+        messagesEndRef.current?.scrollIntoView({ behavior: "smooth" })
+      }
+
+    const messagesEndRef = useRef(null)
+
+    useEffect(() =>{
+        scrollToBottom()
+    },[])
+
+    // useEffect(() => {
+    //     window.addEventListener('scroll', handleScroll);
+    //     return () => window.removeEventListener('scroll', handleScroll);
+    // }, []);
+
+    // useEffect(() => {
+    //     if (!isFetching) return;
+    //     fetchMoreListItems();
+    // }, [isFetching]);
+
+    // const handleScroll = () => {
+    //     if (window.innerHeight + document.documentElement.scrollTop + 5=== document.scrollingElement.scrollHeight) 
+    //     return;
+    //     console.log('Fetch more list items!');
+    //     setIsFetching(true);
+    // }
+
+    // function fetchMoreListItems() {
+    //     console.log(1+1)
+    //   }
+
+    function loadMoreMessages() {
+        if (token != null) {
+            API
+                .graphql(graphqlOperation(messagesByChannelID, {
+                    channelID: '1',
+                    sortDirection: 'DESC',
+                    nextToken: token
+                }))
+                .then((response) => {
+                    let loadMore = response?.data?.messagesByChannelID?.nextToken
+                    setToken(loadMore)
+                    console.log(loadMore)
+
+                    const items = response?.data?.messagesByChannelID?.items;
+
+                    for (let i = items.length - 1; i >= 0; i--) {
+                        if ((items[i].author == currentUserEmail
+                            && items[i].recepient == recipientEmail) || (items[i].recepient == currentUserEmail
+                                && items[i].author == recipientEmail)) {
+
+                            // console.log(items[i])
+                            // setMessages(oldItems => [...oldItems, items[i]])
+
+                        }
+                    }
+                })
+        }
+       
+    }
+
+
+    const handleScroll = (event) => {
+        const { scrollTop, clientHeight, scrollHeight } = event.currentTarget;
+
+        // console.log("ScrollTop: "+scrollTop)
+        // console.log("clientHeight: "+clientHeight)
+        // console.log("scrollHeight: "+scrollHeight)
+        // console.log("")
+        // if (scrollHeight - scrollTop === clientHeight && token!=null) {
+        if (scrollTop === 0 && token!=null) {
+            setPage(prev => prev + 1);
+        }
+    }
+
+    useEffect(() => {
+        const loadUsers = async () => {
+
+
+            API
+                .graphql(graphqlOperation(messagesByChannelID, {
+                    channelID: '1',
+                    sortDirection: 'DESC',
+                    nextToken: token
+
+                }))
+                .then((response) => {
+                    let loadMore = response?.data?.messagesByChannelID?.nextToken
+                    setToken(loadMore)
+
+                    const items = response?.data?.messagesByChannelID?.items;
+
+                    for (let i = 0; i <items.length; i++) {
+                        if ((items[i].author == currentUserEmail
+                            && items[i].recepient == recipientEmail) || (items[i].recepient == currentUserEmail
+                                && items[i].author == recipientEmail)) {
+
+                            setMessages(oldItems => [items[i], ...oldItems])
+
+                        }
+                    }
+                })
+        };
+
+        loadUsers();
+        
+    }, [page]);
 
     return (
         <div className="container">
-                <div className="messages">
-                    <div className="messages-scroller">
+            <div className="messages">
+                {/* {console.log(messages)} */}
+                <div className="messages-scroller" onScroll={handleScroll}>
+                    {messages.map((message) => (
+
+                        <div
+                            key={message.id}
+                            className={message.author === userInfo?.attributes?.email ? 'message me' : 'message'}>{message.body}
+                        </div>
+
+                    ))}
+                      <AlwaysScrollToBottom />
+                      {/* <div ref={messagesEndRef} /> */}
+
+                    {/* <InfiniteScroll
+                        className="messages-scroller"
+                        pageStart={0}
+                        // initialLoad={false}
+                        // isReverse={true}
+                        loadMore={loadMoreMessages}
+                        // hasMore={isFetching}
+                        loader={<div className="loader" key={0}>Loading ...</div>}
+                    >
                         {messages.map((message) => (
 
                             <div
@@ -104,10 +246,13 @@ function Chat({ data, currentUserEmail }) {
                             </div>
 
                         ))}
-                         <AlwaysScrollToBottom />
-                    </div>
+                      
+                    </InfiniteScroll> */}
+
+
                 </div>
-            
+            </div>
+
             <div className="chat-bar">
                 <form onSubmit={handleSubmit}>
                     <input
